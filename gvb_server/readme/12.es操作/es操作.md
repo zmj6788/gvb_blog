7f00066be4f48916c3faef47b6bcbcaa3839a99a
zmj6788 <2124427385@qq.com>
2024-10-29 16:19:12 +0800

    es增删改查

--- gvb_server/readme/12.es操作/es操作.md
+++ gvb_server/readme/12.es操作/es操作.md
@@ -1,6 +1,50 @@
 ### 索引 (Index)
 
 - **定义**：索引是Elasticsearch中用于存储文档的地方。它类似于关系数据库中的表
+相当于表明
 ### 映射 (Mapping)
 
 - **定义**：映射描述了索引中文档的字段及其对应的类型和属性
+相当于规定表内类型的属性
+
+### 创建索引
+
+通过Index和Mapping创建索引
+
+### 删除索引
+
+通过index删除索引
+
+### 创建一条数据
+
+实例化对象通过索引index创建
+
+### 查询数据
+
+通过索引index分页查询
+通过索引index分页查询特定字段
+限制字段使用
+```
+FetchSourceContext(elastic.NewFetchSourceContext(true).Include("title")).
+```
+使用
+```
+Source(`{"_source": ["title"]}`).
+和 
+Source(&elastic.SourceConfig{Includes: []string{"title"}}). 
+均无法使用
+```
+
+### 更新数据
+
+通过数据id查找对应数据更新
+- "_id": "GOQg15IBSFVhxiV54o14",
+### 删除数据
+
+通过数据id删除数据
+```
+count, err := Remove([]string{"GOQg15IBSFVhxiV54o14", "IuRK15IBSFVhxiV5OY1J", "GeQq15IBSFVhxiV5R418", "GuQr15IBSFVhxiV5kY3W"})
+
+  fmt.Println(count, err)
+```
+
--- gvb_server/testdata/es操作/es操作.go
+++ gvb_server/testdata/es操作/es操作.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"gvb_server/core"
 
 	"github.com/olivere/elastic/v7"
@@ -119,8 +121,140 @@ func (demo DemoModel) RemoveIndex() error {
 	return nil
 }
 
-//初始化操作，自动执行
+// Create 创建
+func Create(data *DemoModel) (err error) {
+	indexResponse, err := client.Index().
+		Index(data.Index()).
+		BodyJson(data).Do(context.Background())
+	if err != nil {
+		logrus.Error(err.Error())
+		return err
+	}
+	data.ID = indexResponse.Id
+	return nil
+}
 
+// FindList 列表查询
+func FindList(key string, page, limit int) (demoList []DemoModel, count int) {
+	boolSearch := elastic.NewBoolQuery()
+	from := page
+	if key != "" {
+		boolSearch.Must(
+			elastic.NewMatchQuery("title", key),
+		)
+	}
+	if limit == 0 {
+		limit = 10
+	}
+	if from == 0 {
+		from = 1
+	}
+
+	res, err := client.
+		Search(DemoModel{}.Index()).
+		Query(boolSearch).
+		From((from - 1) * limit).
+		Size(limit).
+		Do(context.Background())
+	if err != nil {
+		logrus.Error(err.Error())
+		return
+	}
+	count = int(res.Hits.TotalHits.Value) //搜索到结果总条数
+	for _, hit := range res.Hits.Hits {
+		var demo DemoModel
+		data, err := hit.Source.MarshalJSON()
+		if err != nil {
+			logrus.Error(err.Error())
+			continue
+		}
+		err = json.Unmarshal(data, &demo)
+		if err != nil {
+			logrus.Error(err)
+			continue
+		}
+		demo.ID = hit.Id
+		demoList = append(demoList, demo)
+	}
+	return demoList, count
+}
+
+func FindSourceList(key string, page, limit int) (demoList []DemoModel, count int) {
+	boolSearch := elastic.NewBoolQuery()
+	from := page
+	if key != "" {
+		boolSearch.Must(
+			elastic.NewMatchQuery("title", key),
+		)
+	}
+	if limit == 0 {
+		limit = 10
+	}
+	if from == 0 {
+		from = 1
+	}
+
+	res, err := client.
+		Search(DemoModel{}.Index()).
+		Query(boolSearch).
+		FetchSourceContext(elastic.NewFetchSourceContext(true).Include("title")).
+		From((from - 1) * limit).
+		Size(limit).
+		Do(context.Background())
+	if err != nil {
+		logrus.Error(err.Error())
+		return
+	}
+	count = int(res.Hits.TotalHits.Value) //搜索到结果总条数
+	demoList = []DemoModel{}
+	for _, hit := range res.Hits.Hits {
+		var demo DemoModel
+		data, err := hit.Source.MarshalJSON()
+		if err != nil {
+			logrus.Error(err.Error())
+			continue
+		}
+		err = json.Unmarshal(data, &demo)
+		if err != nil {
+			logrus.Error(err)
+			continue
+		}
+		demo.ID = hit.Id
+		demoList = append(demoList, demo)
+	}
+	return demoList, count
+}
+
+// Update 更新
+func Update(id string, data *DemoModel) error {
+	_, err := client.
+		Update().
+		Index(DemoModel{}.Index()).
+		Id(id).
+		Doc(map[string]string{
+			"title": data.Title,
+		}).
+		Do(context.Background())
+	if err != nil {
+		logrus.Error(err.Error())
+		return err
+	}
+	logrus.Info("更新demo成功")
+	return nil
+}
+
+// Remove 批量删除
+func Remove(idList []string) (count int, err error) {
+	bulkService := client.Bulk().Index(DemoModel{}.Index()).Refresh("true")
+	for _, id := range idList {
+		req := elastic.NewBulkDeleteRequest().Id(id)
+		bulkService.Add(req)
+	}
+	res, err := bulkService.Do(context.Background())
+	return len(res.Succeeded()), err
+}
+
+// 初始化操作，自动执行
 func init() {
 	// 配置信息读取
 	core.InitConf()
@@ -130,8 +264,25 @@ func init() {
 	client = EsConnect()
 }
 func main() {
-	//索引操作
-	demo := DemoModel{}
-	demo.CreateIndex()
-	demo.RemoveIndex()
+	// 索引操作
+	// demo := DemoModel{ID: "1", Title: "python", UserID: 1,CreatedAt: "2023-04-01 00:00:00"}
+	// demo.CreateIndex()
+	// demo.RemoveIndex()
+
+	// 数据操作
+	// 创建
+	// err := Create(&demo)
+	// if err != nil {
+	// 	logrus.Error(err)
+	// }
+	// 查询
+	// list, count := FindList("python", 0, 10)
+	// fmt.Println(list, count)
+	// list1, count1 := FindSourceList("python", 0, 10)
+	// fmt.Println(list1, count1)
+	// 更新
+	// Update("HuRC15IBSFVhxiV5jY3J", &DemoModel{Title: "go"})
+	// 删除
+	count, err := Remove([]string{"GOQg15IBSFVhxiV54o14", "IuRK15IBSFVhxiV5OY1J", "GeQq15IBSFVhxiV5R418", "GuQr15IBSFVhxiV5kY3W"})
+	fmt.Println(count, err)
 }
