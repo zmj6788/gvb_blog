f2ecb5137c860bebc8736c6bc718a80c59b209b1
zmj6788 <2124427385@qq.com>
2024-10-30 14:28:54 +0800

    article表结构迁移es

--- gvb_server/models/tag_model.go
+++ gvb_server/models/tag_model.go
@@ -4,5 +4,4 @@ package models
 type TagModel struct {
 	MODEL
 	Title    string         `gorm:"size:16" json:"title"`            // 标签名
-	// Articles []ArticleModel `gorm:"many2many:article_tag_models;" json:"-"` // 标签文章
 }
